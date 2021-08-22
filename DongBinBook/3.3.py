edaff5a5cca4a369b723f0e603a1355f5cdc42f4
Jung0Jin <qjsmdk1346@naver.com>
2021-08-22 16:24:51 +0900

    20210822

--- DongBinBook/3.3.py
+++ DongBinBook/3.3.py
@@ -1,7 +1,10 @@
-# # min() 함수를 이용하는 답안 예시
+# 숫자 카드 게임
+
+# # 3-3.py min() 함수를 이용하는 답안 예시
+# # N, M을 공백으로 구분하여 입력받기
 # n, m = map(int, input().split())
 
-# result=0
+# result = 0
 # # 한 줄씩 입력받아 확인
 # for i in range(n):
 #     data = list(map(int, input().split()))
@@ -12,7 +15,7 @@
 
 # print(result) # 최종 답안 출력
 
-# 2중 반복문 구조를 이용하는 답안 예시
+# 3-4.py 2중 반복문 구조를 이용하는 답안 예시
 # N, M을 공백으로 구분하여 입력받기
 n, m = map(int, input().split())
 
@@ -21,7 +24,7 @@ result = 0
 for i in range(n):
     data = list(map(int, input().split()))
     # 현재 줄에서 '가장 작은 수' 찾기
-    min_value = 10001 # 들어오는 수가 1~10000 이다
+    min_value =  10001
     for a in data:
         min_value = min(min_value, a)
     # '가장 작은 수'들 중에서 가장 큰 수 찾기
--- DongBinBook/3.4.py
+++ DongBinBook/3.4.py
@@ -1,9 +1,11 @@
-# # 단순하게 푸는 답안 예시
+# 1이 될 때까지
+
+# # 3-5.py 단순하게 푸는 답안 예시
 # n, k = map(int, input().split())
 # result = 0
-# 
+
 # # N이 K 이상이라면 K로 계속 나누기
-# while n >=k:
+# while n >= k:
 #     # N이 K로 나누어 떨어지지 않는다면 N에서 1씩 빼기
 #     while n % k != 0:
 #         n -= 1
@@ -11,22 +13,22 @@
 #     # K로 나누기
 #     n //= k
 #     result += 1
-# 
+
 # # 마지막으로 남은 수에 대하여 1씩 빼기
 # while n > 1:
 #     n -= 1
 #     result += 1
-# 
+
 # print(result)
 
-# 답안 예시
-# N, K를 공백으로 구분하여 입력받기
+# 3-6.py 답안 예시
+# N.K를 공백으로 구분하여 입력받기
 n, k = map(int, input().split())
 result = 0
 
 while True:
     # (N == K로 나누어떨어지는 수)가 될 때까지 1씩 빼기
-    target = (n//k)*k
+    target = (n // k) * k
     result += (n-target)
     n = target
     # N이 K보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
@@ -34,7 +36,7 @@ while True:
         break
     # K로 나누기
     result += 1
-    n //=k
+    n //= k
 
 # 마지막으로 남은 수에 대하여 1씩 빼기
 result += (n-1)
