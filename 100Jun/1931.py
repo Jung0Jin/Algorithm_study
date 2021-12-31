159b5fb4041ed5fe0a76c4eaaf84a3cde06842c9
Jung0Jin <you@example.com>
2022-01-01 01:30:24 +0900

    20211231

---
+++ 100Jun/1931.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/1931
+
+n = int(input())
+
+time = [[0] * 2 for _ in range(n)]
+for i in range(n):
+    start, end = map(int, input().split())
+    time[i][0] = start
+    time[i][1] = end
+
+time.sort(key=lambda x: (x[1], x[0]))
+
+count = 1
+end_time = time[0][1]
+for i in range(1, n):
+    if time[i][0] >= end_time:
+        count += 1
+        end_time = time[i][1]
+
+print(count)
