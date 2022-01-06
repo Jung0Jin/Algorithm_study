70ac66c1d1a2661fbb31516019481d43d7fe19c6
Jung0Jin <you@example.com>
2022-01-06 23:49:42 +0900

    20220106

---
+++ 100Jun/11053.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/11053
+
+n = int(input())
+num_list = list(map(int, input().split()))
+
+dp = [0 for i in range(n)]
+for i in range(n):
+    for j in range(i):
+        if num_list[i] > num_list[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+print(max(dp))
