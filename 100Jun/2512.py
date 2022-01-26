23f1ebecd0cc76a328fe5bbb64e21c0b560ce829
Jung0Jin <qjsmdk1346@naver.com>
2022-01-27 00:41:34 +0900

    20220127

---
+++ 100Jun/2512.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/2512
+
+n = int(input())
+
+s = list(map(int, input().split()))
+
+m = int(input())
+
+low, high = 0, max(s)
+while low <= high:
+    mid = (low+high) // 2
+    num = 0
+    for i in s:
+        if i >= mid:
+            num += mid
+        else:
+            num += i
+    if num <= m:
+        low = mid + 1
+    else:
+        high = mid - 1
+
+print(high)
