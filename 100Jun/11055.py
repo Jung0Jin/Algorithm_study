8ae17e108261b197ec7e0d3d8966905d70984cbb
Jung0Jin <qjsmdk1346@naver.com>
2022-02-08 09:24:15 +0900

    20220208

---
+++ 100Jun/11055.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/11055
+
+n = int(input())
+lst = list(map(int, input().split()))
+
+dp = [x for x in lst]
+
+for i in range(n):
+    for j in range(i):
+        if lst[i] > lst[j]:
+            dp[i] = max(dp[i], dp[j] + lst[i])
+print(max(dp))
