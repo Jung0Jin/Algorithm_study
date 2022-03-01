7faeacb08f5fae967f0229137e78f970aaf17d31
Jung0Jin <qjsmdk1346@naver.com>
2022-03-01 21:09:03 +0900

    20220301

---
+++ 100Jun/4949.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/4949
+
+while True:
+    s = input()
+    if s == '.':
+        break
+    stk = []
+    temp = True
+    for i in s:
+        if i == '(' or i == '[':
+            stk.append(i)
+        elif i == ')':
+            if not stk or stk[-1] == '[':
+                temp = False
+                break
+            elif stk[-1] == '(':
+                stk.pop()
+        elif i == ']':
+            if not stk or stk[-1] == '(':
+                temp = False
+                break
+            elif stk[-1] == '[':
+                stk.pop()
+    if temp == True and not stk:
+        print('yes')
+    else:
+        print('no')
