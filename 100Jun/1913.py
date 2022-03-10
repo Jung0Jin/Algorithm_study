5c73440601cc0151e50f91490354ffb12cf8f650
Jung0Jin <qjsmdk1346@naver.com>
2022-03-10 23:55:48 +0900

    20220310

--- 100Jun/1913.py
+++ 100Jun/1913.py
@@ -1,36 +1,33 @@
-import sys
+import  copy
 
-input = sys.stdin.readline
 n = int(input())
 m = int(input())
-board = [[0 for _ in range(n)] for _ in range(n)]
-
-dr = [0, 1, 0, -1] # 오른쪽, 아래쪽, 왼쪽, 위쪽 순서
-dc = [1, 0, -1, 0]
-
-r = n//2  # 시작 row
-c = n//2  # 시작 column
-num = 1  # 해당 위치에 들어갈 숫자 1씩 증가 예정
-len = 0  # 특정 방향으로 이동할 길이 얼마나 더할 것인가. for 문으로 동일 작업 수행 가능.
-
-board[r][c] = num
-
-while True:
-    for i in range(4):
-        for _ in range(len):  # 특정 방향으로 한칸씩 이동하며 숫자 입력
-            r+=dr[i]
-            c+=dc[i]
-            num+=1
-            board[r][c]=num
-            if num==m:  # 찾을 번호의 인덱스 저장
-                ans = [r+1, c+1]
-
-    if r==c==0:
-        break
-    r -= 1
-    c -= 1
-    len += 2
+snail = []
+temp = []
 
 for i in range(n):
-    print(*board[i])
-print(*ans)
+    temp.append(0)
+
+for i in range(n):
+    snail.append(copy.deepcopy(temp))
+
+move = [[0,1],[1,0],[0,-1],[-1,0]]
+d = 0
+now_x = 0 ; now_y = 0 ; now_num = n*n
+
+while(now_num > 0):
+    snail[now_y][now_x] = now_num
+    if(now_x + move[d][0] < 0 or now_x + move[d][0] >= n or now_y + move[d][1] < 0 or now_y + move[d][1] >= n or snail[now_y+move[d][1]][now_x+move[d][0]] != 0):
+        d = (d + 1) % 4
+    now_y = now_y + move[d][1]
+    now_x = now_x + move[d][0]
+    now_num = now_num - 1
+
+for i in range(n):
+    for j in range(n):
+        if(snail[i][j] == m):
+            find_x = j
+            find_y = i
+        print(snail[i][j] , end = ' ')
+    print()
+print(find_y + 1,find_x + 1)
